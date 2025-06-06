9c180511efe2d4d1c9b3227a22335d0028930ff6
Webster <strategywebstermanyanga@gmail.com>
2025-06-06 14:55:08 +0200

    feat: add Habit component, dashboard styles, and integrate habit tracking in the dashboard

---
+++ components/habit.tsx
@@ -0,0 +1,16 @@
+export default function Habit() {
+  return (
+    <>
+      <div className=" uppercase border-r border-[#DDD] text-xs">
+        Exercise Every Morning
+      </div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+      <div className=" w-[23px] h-[23px] mx-auto my-auto bg-[#4ADE80] rounded-full"></div>
+    </>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,25 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
+import styles from "./dashboard.module.css";
+import Habit from "../../../components/habit";
+
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
 });
 
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 export default function Dashboard() {
+  const checkboxes = Array(365).fill(
+    <div className="w-full h-full bg-[#4ADE80] "></div>
+  );
   return (
-    <div className={`${inter.className} h-screen w-screen`}>
+    <div className={`${inter.className} h-screen w-screen bg-[#99B3AA]`}>
       <div className="flex justify-between w-full pt-3.5 pr-3.5">
         <Image
           src={"/profile.svg"}
@@ -22,12 +34,70 @@ export default function Dashboard() {
           </button>
         </div>
       </div>
-      <div className="bg-white w-[50%] ml-28 text-black ">
-        <div>Calendar</div>
-        <div className="grid grid-cols-2 grid-rows-12">
-            
+      <div className={`${styles.homeGrid} ${nunito.className} `}>
+        <div className="bg-white w-min ml-28 text-[#111] px-10 rounded-sm">
+          <div className="  w-full flex justify-end p-3.5 ">
+            <button className="text-xs uppercase border border-[#CFCFCF] rounded-sm p-3.5">
+              01 January 2023 - 07 January 2023
+            </button>
+          </div>
+
+          <ul className=" grid grid-cols-[200px_repeat(7,70px)] gap-2 ">
+            <li className=" uppercase text-sm border-b border-[#DDD]">
+              Habits
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Sun</h1>
+              <h2>01</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Mon</h1>
+              <h2>02</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Tue</h1>
+              <h2>03</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Wed</h1>
+              <h2>04</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Thu</h1>
+              <h2>05</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Fri</h1>
+              <h2>06</h2>
+            </li>
+            <li className=" uppercase text-sm text-[#222] border-b border-[#DDD] flex flex-col items-center">
+              <h1>Sat</h1>
+              <h2>07</h2>
+            </li>
+          </ul>
+          <ul className="grid grid-cols-[200px_repeat(7,70px)] gap-2">
+            <Habit />
+            <Habit />
+            <Habit />
+            <Habit />
+            <Habit />
+            <Habit />
+            <Habit />
+          </ul>
+          <div>Streak Grid</div>
+          <div className={`${styles.streakGrid} p-8 justify-center`}>
+            {checkboxes.map((checkbox, i) => (
+              <div className="w-full h-full bg-[#4ADE80]" key={i}></div>
+            ))}
+          </div>
         </div>
-        <div>Streak Grid</div>
+        <Image
+          className=" w-[60%]"
+          src="/plant.png"
+          alt="Plant"
+          width={160}
+          height={200}
+        />
       </div>
     </div>
   );
